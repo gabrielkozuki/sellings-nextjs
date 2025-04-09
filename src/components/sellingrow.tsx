8778475b974f919f07111f4111c6793525d367fa
Gabriel Kozuki <50888200+gabrielkozuki@users.noreply.github.com>
2025-04-09 17:57:40 +0000

    feat: integrate UI modals in Edit and Delete actions

--- src/components/sellingrow.tsx
+++ src/components/sellingrow.tsx
@@ -1,24 +1,62 @@
 import { TableRow, TableCell } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
 import { Pencil } from "lucide-react";
+import { DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog";
+import { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogDescription, AlertDialogFooter, AlertDialogCancel, AlertDialogAction } from "@/components/ui/alert-dialog";
+
+import SellingForm from "@/components/sellingform";
 
 type Props = {
-    selling: {
-        id: number;
-        name: string;
-        price: string;
-    }
+  selling: {
+    id: number;
+    name: string;
+    price: string;
+  }
 }
 
 export default function SellingRow({ selling }: Props) {
-    return (
-        <TableRow className="hover:bg-gray-50">
-            <TableCell className="px-4 py-2 border-t">{selling.name}</TableCell>
-            <TableCell className="px-4 py-2 border-t">{selling.price}</TableCell>
-            <TableCell className="px-4 py-2 border-t flex gap-6 justify-end">
+
+  const handleEdit = () => { };
+
+  const handleDelete = () => { };
+
+  return (
+    <TableRow key={selling.id} className="hover:bg-gray-50">
+      <TableCell className="px-4 py-2 border-t">{selling.name}</TableCell>
+      <TableCell className="px-4 py-2 border-t">{selling.price}</TableCell>
+      <TableCell className="px-4 py-2 border-t flex gap-6 justify-end">
+        <Dialog>
+          <DialogTrigger asChild>
             <Button variant="outline"><Pencil /> Editar</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Editar venda</DialogTitle>
+              <DialogDescription></DialogDescription>
+            </DialogHeader>
+            <SellingForm buttonLabel="Editar" onSubmit={handleEdit} />
+          </DialogContent>
+        </Dialog>
+
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
             <Button variant="link" className="text-red-600">Deletar</Button>
-            </TableCell>
-        </TableRow>
-    )
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Confirmar ação</AlertDialogTitle>
+              <AlertDialogDescription>
+                Você tem certeza que deseja deletar a venda <strong>{selling.name}</strong>? Esta ação não pode ser desfeita.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancelar</AlertDialogCancel>
+              <AlertDialogAction onClick={handleDelete}>Confirmar</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </TableCell>
+    </TableRow>
+  )
 }
